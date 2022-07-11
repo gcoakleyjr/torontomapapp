5dbee81fa97b05748a90328cddd1cf4b7fd6dbbb
Giovanni Coakley <gcoakley.jr@gmail.com>
2022-07-11 00:01:37 -0400

    Started replies.

--- models/review.js
+++ models/review.js
@@ -7,7 +7,11 @@ const reviewSchema = new Schema({
     author: {
         type: Schema.Types.ObjectId,
         ref: "User"  //ref refers to the model. so we have user, review and post model. when we supply an Object ID, it refers to that model and then we we populate it, we get the actual data
-    }
+    },
+    replies: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Review' 
+    }]
 })
 
 module.exports = mongoose.model("Review", reviewSchema)
